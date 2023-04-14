c6ddd61240f050efa67519a4249ae24d2267f2f2
Paul Ajuizeogu <paul.a@mblhightech.com>
2023-04-14 15:59:54 +0300

    Forgot password UI

--- src/app/layout/AuthLayout.tsx
+++ src/app/layout/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./authLayout.less";
 import { Card, Carousel, Col, ConfigProvider, Row, Typography } from "antd";
 import s1 from "../../../public/slide-1.jpg";
@@ -7,15 +7,21 @@ import s3 from "../../../public/slide-3.jpg";
 import s4 from "../../../public/slide-4.jpg";
 import s5 from "../../../public/slide-5.jpg";
 import { authLayoutPrimaryColor } from "../../settings";
+import { paths } from "../../utils/paths";
 
 const AuthLayout = () => {
+  const path = useLocation().pathname;
+  const inForgotPassword = path.includes(paths.forgotPassword);
+
   return (
     <ConfigProvider theme={{ token: { colorPrimary: authLayoutPrimaryColor } }}>
       <main>
         <Row>
           <Col span={12}>
             <Card className="auth-container">
-              <Typography.Title>Welcome</Typography.Title>
+              {!inForgotPassword && (
+                <Typography.Title>Welcome</Typography.Title>
+              )}
               <Outlet />
             </Card>
           </Col>
--- src/modules/auth/ForgotPassword.tsx
+++ src/modules/auth/ForgotPassword.tsx
@@ -1,13 +1,33 @@
-import { Button, Result } from "antd";
+import { Button, Card, Form, Input, Result, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { paths } from "../../utils/paths";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 const ForgotPassword = () => {
   return (
-    <Result
-      status="500"
-      title="500"
-      subTitle="This page is under development"
-      extra={<Button type="primary">Go Back</Button>}
-    />
+    <div>
+      <Typography.Title>Forgot Password?</Typography.Title>
+
+      <Card className="mb-4">
+        <Form layout="vertical">
+          <Form.Item label="E-mail">
+            <Input
+              size="large"
+              required
+              placeholder="Please enter your email address"
+            />
+          </Form.Item>
+
+          <Button disabled size="large" htmlType="submit" type="primary" block>
+            Reset Password
+          </Button>
+        </Form>
+      </Card>
+
+      <Link to={paths.login}>
+        <ArrowLeftOutlined /> Go to login
+      </Link>
+    </div>
   );
 };
 
