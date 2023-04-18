b50e7ca6e649f582e07ab7f842d051e804e7ebbf
Paul Ajuizeogu <paul.a@mblhightech.com>
2023-04-18 14:12:48 +0300

    UI layout skeleton

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,5 +1,24 @@
+import { Layout, Skeleton } from "antd";
+import "./index.less";
+
+const { Sider, Content, Header, Footer } = Layout;
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Layout className="message-container">
+      <Sider className="siderStyle">
+        <Skeleton round avatar active></Skeleton>
+      </Sider>
+      <Layout>
+        <Header className="headerStyle"></Header>
+        <Content className="contentStyle">
+          <Skeleton avatar round active></Skeleton>
+        </Content>
+        <Footer className="footerStyle"></Footer>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default Home;
+
