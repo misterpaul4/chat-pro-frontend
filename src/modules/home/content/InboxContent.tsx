71f7791b3606abed94d6210b87214024c0e71e52
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-05-29 23:52:28 +0300

    Handle chat scroll

--- src/modules/home/content/InboxContent.tsx
+++ src/modules/home/content/InboxContent.tsx
@@ -1,6 +1,7 @@
 import { List } from "antd";
 import { IThread, ThreadTypeEnum } from "../api/types";
 import MessageBox from "./MessageBox";
+import { useEffect, useRef } from "react";
 
 interface IProps {
   thread: IThread;
@@ -9,35 +10,48 @@ interface IProps {
 
 const InboxContent = ({ thread, userId }: IProps) => {
   const { type } = thread;
+  const ref = useRef<HTMLDivElement>(null);
+
+  const chatScroll = () => {
+    if (ref?.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    chatScroll();
+  }, [thread]);
 
   const messageLength = thread.messages.length - 1;
 
   return type === ThreadTypeEnum.Request ? (
     <>Request not approved</>
   ) : (
-    <List
-      dataSource={thread.messages}
-      className="border"
-      renderItem={(_, index) => {
-        const { message, createdAt, senderId } =
-          thread.messages[messageLength - index];
-        const fromUser = senderId === userId;
+    <div ref={ref} id="message-list">
+      <List
+        dataSource={thread.messages}
+        className="px-3"
+        renderItem={(_, index) => {
+          const { message, createdAt, senderId } =
+            thread.messages[messageLength - index];
+          const fromUser = senderId === userId;
 
-        return (
-          <List.Item
-            className={`border-0 py-1 justify-content-${
-              fromUser ? "end" : "start"
-            }`}
-          >
-            <MessageBox
-              createdAt={createdAt}
-              message={message}
-              fromUser={fromUser}
-            />
-          </List.Item>
-        );
-      }}
-    />
+          return (
+            <List.Item
+              className={`border-0 py-1 justify-content-${
+                fromUser ? "end" : "start"
+              }`}
+            >
+              <MessageBox
+                createdAt={createdAt}
+                message={message}
+                fromUser={fromUser}
+              />
+            </List.Item>
+          );
+        }}
+      />
+    </div>
   );
 };
 
