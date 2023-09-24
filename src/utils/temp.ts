18bc2eac81444c27042837c01f9fe7d3ae465d16
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-09-24 17:53:54 +0300

    Deploy app

--- src/utils/temp.ts
+++ src/utils/temp.ts
@@ -1,9 +1,13 @@
 import { IBaseUser } from "../modules/auth/control/types";
 
+const dt = new Date();
+
 export const TEMP_USER: IBaseUser = {
   email: "test@email.com",
   firstName: "test_firstName",
   lastName: "test_lastName",
   id: "032f3992-f4a4-4d88-a855-441ffd6fddc3",
+  createdAt: dt.toISOString(),
+  updatedAt: dt.toISOString(),
 };
 
