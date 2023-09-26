fe238a33797077cafe25e5a9a792e8416da881a8
Paul Ajuizeogu <ajuzepaul@gmail.com>
2023-09-26 21:56:21 +0300

    Fix for deployment

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,8 @@ export const authLayoutPrimaryColor = "#0d0c22";
 export const layoutPrimaryColor = "#ea4c89";
 export const transparentTextColor = "lightgray";
 export const hoverColor = "rgb(247, 247, 247)";
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+// export const BASE_URL = "https://localhost:9287";
+export const BASE_URL = "https://chat-pro-backend.adaptable.app";
 export const THREAD_MEMORY = new Map(); // { messageId as key }, IThreadMemory as type
 export const SETTINGS = {
   maxLastScrolls: 5,
